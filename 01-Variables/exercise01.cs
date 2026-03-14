1c546583bad6113a02ac50799cacd3016308f779
Rui Júnior <dev.ruijunior@gmail.com>
2026-03-14 06:00:24 -0300

    feat: add first variables exercise

---
+++ 01-Variables/exercise01.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Rui Junior";
+        int age = 35;
+        string country = "Brazil";
+
+        Console.WriteLine("Developer Information");
+        Console.WriteLine("---------------------");
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Country: " + country);
+    }
+}
