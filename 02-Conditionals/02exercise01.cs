e805a0968f7151706a03622ba9fded52369edc04
Rui Júnior <dev.ruijunior@gmail.com>
2026-03-17 23:18:57 -0300

    add Number Checker

---
+++ 02-Conditionals/02exercise01.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+static void Main()
+{
+Console.WriteLine("—— Number Checker ——");
+Console.WriteLine("----------------------");
+Console.WriteLine();
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+Console.WriteLine("The number is positive.");
+}
+else if (number < 0)
+{
+Console.WriteLine("The number is negative.");
+}
+else
+{
+Console.WriteLine("The number is zero.");
+}
+
+if (number % 2 == 0)
+{
+Console.WriteLine("The number is even.");
+}
+else
+{
+Console.WriteLine("The number is odd.");
+}
+}
+}
