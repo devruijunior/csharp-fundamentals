b8333a0e4b2ffbe00858f12bbde420581fa8157c
Rui Júnior <dev.ruijunior@gmail.com>
2026-03-21 21:14:23 -0300

    add Simple Menu System

---
+++ 02-Conditionals/02exercise10.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("—— Simple Menu System ——"); 
+        Console.WriteLine("----------------------"); 
+        Console.WriteLine();
+
+        Console.WriteLine("1 - Check Balance ");
+        Console.WriteLine("2 - Deposit "); 
+        Console.WriteLine("3 - Withdraw ");
+        Console.WriteLine(); 
+
+        Console.Write("Choose an option:"); 
+        int inputOption = int.Parse(Console.ReadLine());
+
+        int balance = 1000;
+
+        if (inputOption == 1 )
+        {
+            Console.WriteLine("You Balance is: "+balance);
+        }
+        else if (inputOption == 2 )
+        {
+            Console.WriteLine("Deposit selected");
+        }
+        else if (inputOption == 3 )
+        {
+            Console.WriteLine("Withdraw selected");
+        }
+        else
+        {
+            Console.WriteLine("Invalid option");
+        }
+    }
+}
